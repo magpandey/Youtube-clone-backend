40e19e3bb13930d41d44c57f2c516289de986df4
Vinayak Pandey <pandeyvinayak589@gamil.com>
2026-03-11 21:45:10 +0530

    Add cloudinary utility for image uploads and file management

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import cloudinary from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({
+     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+})
+const uploadOnCloudinary = async(filePath)  => {
+    try {
+
+        if(!filePath){
+            return null
+        }
+        const result = await cloudinary.uploader.upload(filePath, {
+            resource_type: "auto"
+        })
+
+        fs.unlinkSync(filePath)
+        return result
+    } catch (error) {
+        fs.unlinkSync(filePath)
+        return null;
+    }
+}
